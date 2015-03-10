ec1cf4ec3c88835de9df14890aa2656a417d0bcc
Gerald LONLAS <g.lonlas@gmail.com>
2015-03-09 21:10:00 -0400

    Enable HTTP Cache on Prod

--- web/app.php
+++ web/app.php
@@ -16,11 +16,11 @@ $apcLoader->register(true);
 */
 
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', false);
+$kernel = new AppKernel('prod', true);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
