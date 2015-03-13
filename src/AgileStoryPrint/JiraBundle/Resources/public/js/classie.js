45786aa0544ea673fbaa92a09f3e984c099ecc23
Gerald LONLAS <g.lonlas@gmail.com>
2015-03-13 16:06:00 -0400

    Try to speed up the page load

--- src/AgileStoryPrint/JiraBundle/Resources/public/js/classie.js
+++ src/AgileStoryPrint/JiraBundle/Resources/public/js/classie.js
@@ -1,12 +1,5 @@
-/*!
- * classie - class helper functions
- * from bonzo https://github.com/ded/bonzo
- * 
- * classie.has( elem, 'my-class' ) -> true/false
- * classie.add( elem, 'my-new-class' )
- * classie.remove( elem, 'my-unwanted-class' )
- * classie.toggle( elem, 'my-class' )
- */
+// classie - class helper functions
+// from bonzo https://github.com/ded/bonzo
 
 /*jshint browser: true, strict: true, undef: true */
 /*global define: false */
